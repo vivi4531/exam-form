a955fbd16a01d346a7bc5ebce4561279b793efb9
vivimortensen <vivi4531@stud.kea.dk>
2021-06-03 11:29:40 +0200

    submit button disabled removed

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import "./sass/style.scss";
 import "./form.js"; 
 import "./cardvalidation.js"; 
 
-//Enable submit button after validation
-const form = document.getElementById('orderpayment'); 
-form.addEventListener("change", () => {
-    document.getElementsById('orderbutton').disabled = !form.checkValidity(); 
-}); 
+// //Enable submit button after validation
+// const form = document.getElementById('orderpayment'); 
+// form.addEventListener("change", () => {
+//     document.getElementsById('orderbutton').disabled = !form.checkValidity(); 
+// }); 
