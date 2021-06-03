100c5a527793f7dbab692bdde1b41927b96a5eed
vivimortensen <vivi4531@stud.kea.dk>
2021-06-03 11:23:07 +0200

    ipad optimize

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import "./sass/style.scss";
 import "./form.js"; 
 import "./cardvalidation.js"; 
+
+//Enable submit button after validation
+const form = document.getElementById('orderpayment'); 
+form.addEventListener("change", () => {
+    document.getElementsById('orderbutton').disabled = !form.checkValidity(); 
+}); 
